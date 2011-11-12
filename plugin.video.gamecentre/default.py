f0476172d15a082a86da59086004506b6b2bdc6a
Chris <cjsimpson@gmail.com>
2011-11-12 15:49:27 -0500

    Adding pydef files for xbmc.

---
+++ plugin.video.gamecentre/default.py
@@ -0,0 +1,29 @@
+import xbmcgui
+import xbmcplugin
+import sys
+
+# magic; id of this plugin's insta nce - cast to inte ger
+thisPlugin = int(sys.argv [1])
+
+def create_listings():
+    """
+    Creates a listing that XBMC can display as a directory listing
+    @return list
+    """
+    listing = []
+    #TODO: Pull the data from the scraper
+    listing.append('Test 1')
+    return listing
+
+def send_to_xbmc(listing):
+    global thisPlugin
+    
+    for item in listing:
+        listItem = xbmcgui.ListItem(item)
+        xbmcplugin.addDirectoryItem(thisPlugin, '', listItem)
+    
+    xbmcplugin.endOfDirectory(thisPlugin)
+
+send_to_xbmc( create_listings() )
+
+
